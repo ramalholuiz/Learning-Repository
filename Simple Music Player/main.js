4bf6db7234797204b9f7f21040082e7045d46cfa
randradee <dev.renatopandrade@gmail.com>
2021-11-11 11:04:06 -0300

    addded new buttons

--- Simple Music Player/main.js
+++ Simple Music Player/main.js
@@ -11,4 +11,29 @@ function pauseTrack() {
 function stopTrack() {
   music.pause()
   music.currentTime = 0
+  music.playbackRate = 0
+}
+
+function backTime() {
+  music.currentTime -= 5
+}
+
+function fwdTime() {
+  music.currentTime += 5
+}
+
+function decreaseSpd() {
+  music.playbackRate -= 0.2
+}
+
+function increaseSpd() {
+  music.playbackRate += 0.2
+}
+
+function volDown() {
+  music.volume -= 0.1
+}
+
+function volUp() {
+  music.volume += 0.1
 }
