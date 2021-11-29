001e7cbc802469e5e083fc8b709ca65a168889ed
randradee <dev.renatopandrade@gmail.com>
2021-11-29 12:27:52 -0300

    testing an api

--- Random Tests/scripts/test.js
+++ Random Tests/scripts/test.js
@@ -1,21 +1,45 @@
-function Book(title, pages, isbn) {
-  this.title = title;
-  this.pages = pages;
-  this.isbn = isbn;
-  this.printIsbn = function () {
-    console.log(this.isbn);
-  };
+const summonerName = "alvinl";
+
+const url = `https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=RGAPI-1b5d23be-b04f-4d16-9889-bb48cf375f36`;
+
+function getData(summonerName, url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      let puuid = data.puuid;
+      let url2 = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=RGAPI-1b5d23be-b04f-4d16-9889-bb48cf375f36`;
+
+      fetch(url2)
+        .then(response => response.json())
+        .then(data => {
+          const matches = data;
+          for (i of matches) {
+            urlMatch = `https://americas.api.riotgames.com/lol/match/v5/matches/${i}?api_key=RGAPI-1b5d23be-b04f-4d16-9889-bb48cf375f36`;
+
+            fetch(urlMatch)
+              .then(response => response.json())
+              .then(data => {
+                let matchData = data;
+
+                for (let i = 0; i < 10; i++) {
+                  let playerName = matchData.info.participants[i].summonerName;
+                  if (playerName === "Alvin L") {
+                    console.log(matchData.info.participants[i].championName);
+                  }
+                }
+              })
+              .catch(err => {
+                console.log("Erro no fetch do match: " + err);
+              });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
-let book1 = new Book(
-  "Estruturas de dados e algoritmos com javascript",
-  406,
-  "978-85-7522-693-3"
-);
-console.log(book1);
-
-Book.prototype.printTitle = function () {
-  console.log(this.title);
-};
-book1.printTitle();
-book1.printIsbn();
+getData(summonerName, url);
