b6d4b5c0cbe0c8cd2f87444497137d29db5ce84c
randradee <dev.renatopandrade@gmail.com>
2021-12-15 10:39:25 -0300

    developed a simple server with some error treatment and serving of files

--- Node_Crash_Course/index.js
+++ Node_Crash_Course/index.js
@@ -5,42 +5,98 @@ const fs = require("fs");
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile(
-      path.join(__dirname, "public", "index.html"),
-      (error, content) => {
-        if (error) throw error;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
-    );
-  }
-  if (req.url === "/about") {
-    fs.readFile(
-      path.join(__dirname, "public", "about.html"),
-      (error, content) => {
-        if (error) throw error;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
-    );
+  // if (req.url === "/") {
+  //   fs.readFile(
+  //     path.join(__dirname, "public", "index.html"),
+  //     (error, content) => {
+  //       if (error) throw error;
+  //       res.writeHead(200, { "Content-Type": "text/html" });
+  //       res.end(content);
+  //     }
+  //   );
+  // }
+  // if (req.url === "/about") {
+  //   fs.readFile(
+  //     path.join(__dirname, "public", "about.html"),
+  //     (error, content) => {
+  //       if (error) throw error;
+  //       res.writeHead(200, { "Content-Type": "text/html" });
+  //       res.end(content);
+  //     }
+  //   );
+  // }
+
+  // if (req.url === "/api/users") {
+  //   const users = [
+  //     {
+  //       name: "Bob Smith",
+  //       age: "40",
+  //     },
+  //     {
+  //       name: "Renato Andrade",
+  //       age: "29",
+  //     },
+  //   ];
+
+  //   res.writeHead(200, { "Content-Type": "application/json" });
+  //   res.end(JSON.stringify(users));
+  // }
+
+  // Build file path
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  // Extension of the file
+  let extName = path.extname(filePath);
+
+  // Initial content type
+  let contentType = "text/html";
+
+  // Check ext an set content type
+  switch (extName) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application.json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
   }
 
-  if (req.url === "/api/users") {
-    const users = [
-      {
-        name: "Bob Smith",
-        age: "40",
-      },
-      {
-        name: "Renato Andrade",
-        age: "29",
-      },
-    ];
-
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(users));
-  }
+  // Read File
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code == "ENOENT") {
+        // Page not found
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (error, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        // Some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${error.code}`);
+      }
+    } else {
+      // Success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
