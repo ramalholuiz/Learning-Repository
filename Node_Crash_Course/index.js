83396b5fc6ce189dfa0cf4ad8aa8597d907fcb02
randradee <dev.renatopandrade@gmail.com>
2021-12-15 10:21:36 -0300

    learning to serve HTTP and a RESTful API

--- Node_Crash_Course/index.js
+++ Node_Crash_Course/index.js
@@ -1,10 +1,46 @@
-const Logger = require("./logger");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const logger = new Logger();
+const PORT = process.env.PORT || 5000;
 
-logger.on("message", data => console.log("Called Listener:", data));
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (error, content) => {
+        if (error) throw error;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+  if (req.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "public", "about.html"),
+      (error, content) => {
+        if (error) throw error;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
 
-logger.log("Hello World");
-logger.log("Hehe");
-logger.log("test");
-logger.log("dale");
+  if (req.url === "/api/users") {
+    const users = [
+      {
+        name: "Bob Smith",
+        age: "40",
+      },
+      {
+        name: "Renato Andrade",
+        age: "29",
+      },
+    ];
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
