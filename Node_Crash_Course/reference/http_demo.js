4d9ddf2e0a6cb1eed426dffef13e831635e31c3d
randradee <dev.renatopandrade@gmail.com>
2021-12-15 09:58:20 -0300

    running my first http server ever :)

---
+++ Node_Crash_Course/reference/http_demo.js
@@ -0,0 +1,11 @@
+const { read } = require("fs");
+const http = require("http");
+
+// Create server object
+http
+  .createServer((request, response) => {
+    // Write response
+    response.write("Hello World!");
+    response.end();
+  })
+  .listen(5000, () => console.log("Server running"));
