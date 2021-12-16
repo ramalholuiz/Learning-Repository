8a9b9f0a8dcde3fa4eaa3e7ae061c81cd386599e
randradee <dev.renatopandrade@gmail.com>
2021-12-16 08:42:30 -0300

    logging the request

--- EspressJS_Crash_Course/index.js
+++ EspressJS_Crash_Course/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const path = require("path");
-const members = require("./members.js");
+const moment = require("moment");
+const members = require("./Members.js");
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+const app = express();
+
+const logger = (req, res, next) => {
+  console.log(
+    `${req.protocol}://${req.get("host")}${
+      req.originalUrl
+    }: ${moment().format()}`
+  );
+  next();
+};
+
+// Init middleware
+app.use(logger);
 
 // Gets all members
 app.get("/api/members", (req, res) => res.json(members));
