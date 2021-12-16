dc670326591c3312950cb58fe63c0a85a325d6d9
randradee <dev.renatopandrade@gmail.com>
2021-12-16 08:12:41 -0300

    added static folder and another route

--- EspressJS_Crash_Course/index.js
+++ EspressJS_Crash_Course/index.js
@@ -4,8 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(5000, () => console.log(`Server started on port ${PORT}`));
