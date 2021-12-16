9ece8b7817cf9c3cab2fc994bc5c4c1336dc6573
randradee <dev.renatopandrade@gmail.com>
2021-12-16 08:07:52 -0300

    serving a index.html page

--- EspressJS_Crash_Course/index.js
+++ EspressJS_Crash_Course/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(5000, () => console.log(`Server started on port ${PORT}`));
