218609b7109376209048436894c09b09eaeb0d68
randradee <dev.renatopandrade@gmail.com>
2021-12-16 08:35:46 -0300

    serving json

--- EspressJS_Crash_Course/index.js
+++ EspressJS_Crash_Course/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
+const members = require("./members.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Gets all members
+app.get("/api/members", (req, res) => res.json(members));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
