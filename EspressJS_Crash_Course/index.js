8d0fb446721ef5c082c2efc859ff59926a5977ac
randradee <dev.renatopandrade@gmail.com>
2021-12-16 09:04:08 -0300

    coded a get member by id

--- EspressJS_Crash_Course/index.js
+++ EspressJS_Crash_Course/index.js
@@ -1,7 +1,6 @@
 // Import modules
 const express = require("express");
 const path = require("path");
-const moment = require("moment");
 // Import custom modules
 const members = require("./Members.js");
 const logger = require("./middleware/logger");
@@ -14,6 +13,15 @@ app.use(logger);
 
 // Gets all members
 app.get("/api/members", (req, res) => res.json(members));
+// Get single member
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
